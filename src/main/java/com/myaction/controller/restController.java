f776a23e51828eeb1af99bcea1fd1cafa6817b5a
manjaeng <manjaeng@gmail.com>
2018-02-07 17:51:26 +0900

    chatbot client

--- src/main/java/com/myaction/controller/restController.java
+++ src/main/java/com/myaction/controller/restController.java
@@ -1,10 +1,14 @@
 package com.myaction.controller;
 
 
+import com.myaction.domain.mongo.Domain;
+import com.myaction.domain.mongo.DomainRepository;
 import com.myaction.dto.ResultDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,9 +19,12 @@ public class restController {
 
     private static final Logger log = LoggerFactory.getLogger(restController.class);
 
+    @Autowired
+    DomainRepository domainRepository;
 
     @RequestMapping(value = "/api/deliver", method = RequestMethod.POST)
     public ResponseEntity<ResultDTO> deliver() {
+        log.debug("11");
         ResultDTO resultVO = new ResultDTO();
 
         StringBuffer contentBuffer = new StringBuffer();
@@ -39,6 +46,17 @@ public class restController {
         }
         return new ResponseEntity<ResultDTO>(resultVO, HttpStatus.OK);
     }
+
+    @RequestMapping(value = "/api/mongotest", method = RequestMethod.POST, consumes =  MediaType.APPLICATION_JSON_VALUE)
+    public Domain mongotest(@RequestBody Domain domain) {
+        log.debug("22");
+        ResultDTO resultVO = new ResultDTO();
+
+        Domain save = domainRepository.save(domain);
+
+        return save;
+    }
+
 
 
 }
